31fa5f9fec7fa02490572afae210b9954da87daf
Boylees <shenmiheiyirenx@gmail.com>
2024-09-19 10:08:44 +0800

    sqrtx

---
+++ array/sqrtx/src/Solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+
+    public int mySqrt(int x) {
+        // 特殊值判断
+        if (x == 0) {
+            return 0;
+        }
+        if (x == 1) {
+            return 1;
+        }
+
+        int left = 1;
+        int right = x / 2;
+        // 在区间 [left..right] 查找目标元素
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;// 可以保证不溢出，并且向上取整
+            // 注意：这里为了避免乘法溢出，改用除法
+            if (mid > x / mid) {
+                // 下一轮搜索区间是 [left..mid - 1]
+                right = mid - 1;
+            } else {
+                // 下一轮搜索区间是 [mid..right]
+                left = mid;
+            }
+        }
+        return left;
+    }
+}
+
